19502f77f65ab714e80c1d4e5a7c10fb4c3aaea1
shariful-alam-2024 <outsourcingshorif2024@gmail.com>
2024-06-22 23:32:59 +0600

    js- first-assignment

---
+++ name.js
@@ -0,0 +1,95 @@
+let  carname  ="Volvo"  //variable name is:carname,and string value is "Volvo"
+console.log(carname);  //this is output statement
+
+
+
+
+
+//let  carname  ="Volvo"  //variable name is:carname,and string value is "Volvo"
+//console.log(carname);  //this is output statement
+
+
+
+
+// let x=10;
+// let y=5;              //question 3
+// let z= x*=y;
+// console.log(z);
+
+
+// let length =16;            //number    data type    question 4
+// let lastName ="johnson";    // string   data type
+// const x= {
+
+//       firstName:"john",
+//       lastName:"Doe"
+// };       
+
+
+// function myFunction(){        //question 5
+//       alert("Hello World!");
+// }
+// myFunction();
+
+
+// const person  ={                 //question 6
+//       name:"john",age:50
+// };
+// alert(person.name  +  " is  " +  person.age);
+
+
+// function tryToFix(){
+//       alert('try to fix');
+                                  //question 7
+//       }
+    
+
+
+
+
+
+
+
+
+// const cars =["Volvo","jeep","Mercedes"];
+// alert(cars.length);
+
+
+                              //question 8
+
+
+// //question number :2
+
+
+// const brand=["Volvo","jeep","Mercedes"];
+// brand.unshift("Ford");
+// console.log(brand);
+
+// question number:1
+
+
+// let x=Math.random();    //question 9
+// console.log(x);
+// //question number:2
+
+// let x1=Math.max(10,20);
+
+// console.log(x1);
+
+// //question number:3
+
+// let x3=Math.sqrt(9);
+
+// console.log(x3);
+
+
+
+
+
+// let x=10;
+// let y=5;                  //question 10
+// alert(x>y);//x>y
+// //question number:2
+// var age =n;
+// var Age = (age<18) ?"Too Young":"old enough";
+// alert(Age);
